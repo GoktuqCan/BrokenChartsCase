a1ed335d03c75425058bd9ea90539dd2cbb91eb9
GoktuqCan <goktugcancakmak94@gmail.com>
2023-07-23 17:48:09 +0300

    Add navigation infrastructure

---
+++ src/navigation/AppStack.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BottomTabs from './BottomTabs';
+import ChatScreen from 'screens/ChatScreen/ChatScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="BottomTabs" component={BottomTabs} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+    </Stack.Navigator>
+  );
+}
---
+++ src/navigation/BottomTabs.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from 'screens/HomeScreen/HomeScreen';
+import SearchScreen from 'screens/SearchScreen/SearchScreen';
+import HistoryScreen from 'screens/HistoryScreen/HistoryScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="History" component={HistoryScreen} />
+    </Tab.Navigator>
+  );
+}
