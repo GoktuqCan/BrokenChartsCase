974050e1c04db04de8d94aace89d315d589e002d
GoktuqCan <goktugcancakmak94@gmail.com>
2023-07-24 14:35:03 +0300

    Fix colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,7 +1,11 @@
 export const colors = {
-  textPrimary: '#231F20',
-  textSeconday: '#999A9C',
-  bgPrimary: '#FFFFFF',
-  bgSecondary: '#FAFAFD',
-  black: '#000000',
+  textPrimary: 'rgba(35, 31, 32, 1)',
+  textSeconday: 'rgba(153, 154, 156, 1)',
+  bgPrimary: 'rgba(255, 255, 255, 1)',
+  bgSecondary: 'rgba(250, 250, 253, 1)',
+  bgSeparator: 'rgba(0, 0, 0, 0.15)',
+  black: 'rgba(0, 0, 0, 1)',
+  white: 'rgba(255, 255, 255, 1)',
+  sliderBlue: 'rgba(0, 152, 253, 1)',
+  sliderGrey: 'rgba(0, 0, 0, 0.2)',
 };
