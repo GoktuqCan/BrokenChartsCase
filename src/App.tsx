216aec9f09488274f4b5a8204dd51570d29d89b7
GoktuqCan <goktugcancakmak94@gmail.com>
2023-07-26 18:12:42 +0300

    Add persist for history

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as ReduxProvider } from 'react-redux';
 import Navigation from 'navigation/Navigation';
-import store from 'state/store';
+import store, { persistor } from 'state/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ReduxProvider store={store}>
-        <Navigation />
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
       </ReduxProvider>
     </SafeAreaProvider>
   );
--- src/state/store.ts
+++ src/state/store.ts
@@ -3,8 +3,35 @@ import thunk from 'redux-thunk';
 import homeReducer from './home/reducer';
 import searchReducer from './search/reducer';
 import chatReducer from './chat/reducer';
+import historyReducer, {
+  HistoryState,
+  historyInitialState,
+} from './history/reducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  PersistConfig,
+  Persistor,
+  createTransform,
+  persistReducer,
+  persistStore,
+} from 'redux-persist';
 
 const middlewares = [thunk];
+
+const historyTransform = createTransform(
+  (historyState: HistoryState) => {
+    return { ...historyInitialState, data: historyState.data };
+  },
+  null,
+  { whitelist: ['history'] },
+);
+
+const persistConfig: PersistConfig<any> = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['history'],
+  transforms: [historyTransform],
+};
 
 if (__DEV__) {
   const createDebugger = require('redux-flipper').default;
@@ -15,8 +42,13 @@ const rootReducer = combineReducers({
   home: homeReducer,
   search: searchReducer,
   chat: chatReducer,
+  history: historyReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(...middlewares));
+
+export const persistor: Persistor = persistStore(store);
 
 export default store;
